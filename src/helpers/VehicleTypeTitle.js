38550ae470a8971c45a36ca7367bed24b575b6ff
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-10 23:34:59 -0300

    Return vehicle type label

---
+++ src/helpers/VehicleTypeTitle.js
@@ -0,0 +1,11 @@
+export default type => {
+  const types = {
+    1: 'Caminhão 3/4',
+    2: 'Caminhão Toco',
+    3: 'Caminhão ​Truck',
+    4: 'Carreta Simples',
+    5: 'Carreta Eixo Extendido',
+  };
+
+  return types[type];
+};
