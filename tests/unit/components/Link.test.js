27fc039027318837ff7b42dd8751aff31c4c0665
Diego Victor <diegovictorgonzaga@gmail.com>
2022-07-17 01:02:34 -0300

    Update component test

--- tests/unit/components/Link.test.js
+++ tests/unit/components/Link.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
-import faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 import history from '~/services/history';
 import Link from '~/components/Link';
 
 describe('Link component', () => {
   it('should be able to mark the link as active', () => {
-    const link_label = faker.random.word();
+    const link_label = faker.lorem.word();
     const { container } = render(
       <Router history={history}>
         <Link to="/">{link_label}</Link>
