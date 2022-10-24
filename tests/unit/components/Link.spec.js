fa0788a8cd19163ecdf05685f93c201afff2e134
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-10-24 18:19:27 -0300

    Move and rename unit tests

--- tests/unit/components/Link.spec.js
+++ tests/unit/components/Link.spec.js
@@ -8,10 +8,10 @@ import Link from '~/components/Link';
 
 describe('Link component', () => {
   it('should be able to mark the link as active', () => {
-    const link_label = faker.lorem.word();
+    const linkLabel = faker.random.word();
     const { container } = render(
       <Router history={history}>
-        <Link to="/">{link_label}</Link>
+        <Link to="/">{linkLabel}</Link>
       </Router>
     );
 
