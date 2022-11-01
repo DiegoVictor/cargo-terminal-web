e3fd7976e6190ff2d61c6b0ee09df127fe854666
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-01 20:31:00 -0300

    Update routes component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import history from '~/services/history';
 import Drivers from '~/pages/Drivers';
 import Vehicles from '~/pages/Vehicles';
 import Terminal from '~/pages/Terminal';
 import Travels from '~/pages/Travels';
+import Menu from '~/components/Menu';
 
-export default () => (
-  <Router history={history}>
-    <Route path="/" exact component={Terminal} />
-    <Route path="/travels" component={Travels} />
-    <Route path="/vehicles" component={Vehicles} />
-    <Route path="/drivers" component={Drivers} />
-  </Router>
-);
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+
+      <Switch>
+        <Route path="/" exact component={Terminal} />
+        <Route path="/drivers" component={Drivers} />
+        <Route path="/travels" component={Travels} />
+        <Route path="/vehicles" component={Vehicles} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
