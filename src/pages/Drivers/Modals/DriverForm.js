c4b75c395f66255863b53d5a3a8a30312396e7ed
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-03 17:08:14 -0300

    Create validation

--- src/pages/Drivers/Modals/DriverForm.js
+++ src/pages/Drivers/Modals/DriverForm.js
@@ -1,4 +1,16 @@
 import React, { useCallback } from 'react';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required(),
+  cpf: Yup.string().required(),
+  phone: Yup.string().required(),
+  birthday: Yup.string().required(),
+  cnh_number: Yup.string().required(),
+  cnh_type: Yup.string().required(),
+  gender: Yup.string().required(),
+});
+
 function DriverForm({ vehicles, driver, show, cancel, save }) {
 }
 export default DriverForm;
