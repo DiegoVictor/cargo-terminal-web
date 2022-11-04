f9e451c50e936a0ddc19c268ec2e28c69330e2dd
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-04 19:19:58 -0300

    Send data to the api

--- src/pages/Drivers/Modals/DriverForm.js
+++ src/pages/Drivers/Modals/DriverForm.js
@@ -1,5 +1,7 @@
 import React, { useCallback } from 'react';
 import * as Yup from 'yup';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
 
 const schema = Yup.object().shape({
   name: Yup.string().required(),
@@ -12,6 +14,25 @@ const schema = Yup.object().shape({
 });
 
 function DriverForm({ vehicles, driver, show, cancel, save }) {
+  const handleDriverForm = useCallback(
+    async (data) => {
+      try {
+        if (driver._id) {
+          const response = await api.put(`/drivers/${driver._id}`, data);
+          save(response.data);
+          toast.success('Motorista atualizado com sucesso!');
+        } else {
+          const response = await api.post('drivers', data);
+          save(response.data);
+          toast.success('Motorista criado com sucesso!');
+        }
+      } catch (err) {
+        toast.error('Não foi possivel criar o novo motorista');
+      }
+    },
+    [driver._id, save]
+  );
+
 }
 DriverForm.defaultProps = {
   driver: null,
