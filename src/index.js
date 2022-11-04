114477e3bd91b936e38e1f2429de85f98359adbe
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-04 19:20:55 -0300

    Use App component as the mains entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Navigation from '~/routes';
+import App from '~/App';
 
-ReactDOM.render(<Navigation />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
