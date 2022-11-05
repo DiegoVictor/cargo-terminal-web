9a48cffe76d45ae5683f0945e7f6f141d05f7ca0
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-05 00:31:03 -0300

    Set component prop types

--- src/pages/Terminal/Modals/ArrivalForm.js
+++ src/pages/Terminal/Modals/ArrivalForm.js
@@ -1,4 +1,28 @@
 import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
 function ArrivalForm({ arrival, drivers, vehicles, show, cancel, save }) {
 }
+
+ArrivalForm.propTypes = {
+  drivers: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  vehicles: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      model: PropTypes.string.isRequired,
+      type: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  cancel: PropTypes.func.isRequired,
+  save: PropTypes.func.isRequired,
+  arrival: PropTypes.shape({
+    _id: PropTypes.string,
+  }).isRequired,
+  show: PropTypes.bool.isRequired,
+};
+
 export default ArrivalForm;
