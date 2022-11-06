8ddc4de0874c6c8235b96beaa898d78fa5007405
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-06 00:53:22 -0300

    Send arrival to the API

--- src/pages/Terminal/Modals/ArrivalForm.js
+++ src/pages/Terminal/Modals/ArrivalForm.js
@@ -1,6 +1,9 @@
 import React, { useCallback } from 'react';
 import * as Yup from 'yup';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
 
 const schema = Yup.object().shape({
   filled: Yup.string().required(),
@@ -11,6 +14,29 @@ const schema = Yup.object().shape({
 });
 
 function ArrivalForm({ arrival, drivers, vehicles, show, cancel, save }) {
+  const handleTerminalForm = useCallback(
+    (data) => {
+      (async () => {
+        try {
+          if (arrival._id) {
+            const response = await api.put(`/arrivals/${arrival._id}`, data);
+            save(response.data);
+
+            toast.success('Registro atualizado com sucesso!');
+          } else {
+            const response = await api.post('arrivals', data);
+            save(response.data);
+
+            toast.success('Registro criado com sucesso!');
+          }
+        } catch (err) {
+          toast.error('Não foi possivel criar o novo registro');
+        }
+      })();
+    },
+    [arrival._id, save]
+  );
+
 }
 
 ArrivalForm.propTypes = {
