056219312cdbed0ec8c51dcf98ed0952c8db7efa
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-09 18:42:41 -0300

    Update page

--- src/pages/Travels/index.js
+++ src/pages/Travels/index.js
@@ -5,6 +5,7 @@ import { Table, Spinner } from 'react-bootstrap';
 import api from '~/services/api';
 import Description from '~/components/Description';
 import VehicleTypeTitle from '~/helpers/VehicleTypeTitle';
+import { Container, Center } from './styles';
 
 function Travel() {
   const [travels, setTravels] = useState([]);
@@ -21,64 +22,65 @@ function Travel() {
   }, []);
 
   return (
-    <Layout>
-      <Container>
-        <Table striped hover size="sm">
-          <thead>
+    <Container>
+      <Table striped hover size="sm">
+        <thead>
+          <tr>
+            <th>Origem</th>
+            <th>Destino</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
             <tr>
-              <th>Origem</th>
-              <th>Destino</th>
+              <td colSpan="7">
+                <Center>
+                  <Spinner animation="border" />
+                </Center>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {loading ? (
-              <tr>
-                <td colSpan="7">
-                  <Center>
-                    <Spinner animation="border" />
-                  </Center>
-                </td>
-              </tr>
-            ) : (
-              <>
-                {travels.map((travel, i) => (
-                  <Fragment key={i}>
-                    <tr>
-                      <th colSpan="2">{VehicleTypeTitle(travel.type)}</th>
+          ) : (
+            <>
+              {travels.map((travel) => (
+                <Fragment key={travel.type}>
+                  <tr>
+                    <th colSpan="2">{VehicleTypeTitle(travel.type)}</th>
+                  </tr>
+                  {travel.origins.map((origin, i) => (
+                    <tr
+                      key={`${travel.origins.join(
+                        ''
+                      )}${travel.destinations.join('')}`}
+                    >
+                      <td>
+                        <Description>Origem</Description>
+                        <Link
+                          to={`${googleMapUrl + origin[1]},${origin[0]}`}
+                          target="_blank"
+                        >
+                          {origin.reverse().join(', ')}
+                        </Link>
+                      </td>
+                      <td>
+                        <Description>Destino</Description>
+                        <Link
+                          to={`${googleMapUrl + travel.destinations[i][1]},${
+                            travel.destinations[i][0]
+                          }`}
+                          target="_blank"
+                        >
+                          {travel.destinations[i].reverse().join(', ')}
+                        </Link>
+                      </td>
                     </tr>
-                    {travel.origins.map((origin, j) => (
-                      <tr key={j}>
-                        <td>
-                          <Description>Origem</Description>
-                          <Link
-                            to={`${google_map_url + origin[1]},${origin[0]}`}
-                            target="_blank"
-                          >
-                            {origin.reverse().join(', ')}
-                          </Link>
-                        </td>
-                        <td>
-                          <Description>Destino</Description>
-                          <Link
-                            to={`${google_map_url +
-                              travel.destinations[j][1]},${
-                              travel.destinations[j][0]
-                            }`}
-                            target="_blank"
-                          >
-                            {travel.destinations[j].reverse().join(', ')}
-                          </Link>
-                        </td>
-                      </tr>
-                    ))}
-                  </Fragment>
-                ))}
-              </>
-            )}
-          </tbody>
-        </Table>
-      </Container>
-    </Layout>
+                  ))}
+                </Fragment>
+              ))}
+            </>
+          )}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
