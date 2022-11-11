1669b6495620951c165053c26d6a9d9a0a8e71bb
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-11 00:30:54 -0300

    Update component body

--- src/pages/Vehicles/Modals/VehicleForm.js
+++ src/pages/Vehicles/Modals/VehicleForm.js
@@ -1,9 +1,19 @@
 import React, { useCallback } from 'react';
+import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { toast } from 'react-toastify';
+import {
+  Button as Btn,
+  ButtonGroup as BtnGroup,
+  Form as Frm,
+} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 import api from '~/services/api';
+import Input from '~/components/Input';
+import Select from '~/components/Select';
+import Modal from '~/components/Modal';
+
 const schema = Yup.object().shape({
   model: Yup.string().required(),
   type: Yup.string().required(),
@@ -34,6 +44,43 @@ function VehicleForm({ vehicle, save, cancel }) {
     [vehicle, save]
   );
 
+  return (
+    <Modal show={!!vehicle} onHide={cancel} title="Veículo">
+      {vehicle && (
+        <Form
+          initialData={vehicle}
+          schema={schema}
+          onSubmit={handleVehicleForm}
+        >
+          <Frm.Group>
+            <Frm.Label>Modelo</Frm.Label>
+            <Input className="form-control" name="model" placeholder="Modelo" />
+          </Frm.Group>
+
+          <Frm.Group>
+            <Frm.Label>Tipo</Frm.Label>
+            <Select className="form-control" name="type" placeholder="Tipo">
+              <option value="1">Caminhão 3/4</option>
+              <option value="2">Caminhão Toco</option>
+              <option value="3">Caminhão Truck</option>
+              <option value="4">Carreta Simples</option>
+              <option value="5">Carreta Eixo Extendido</option>
+            </Select>
+          </Frm.Group>
+
+          <BtnGroup>
+            <Btn data-testid="cancel" variant="secondary" onClick={cancel}>
+              Cancelar
+            </Btn>
+            <Btn data-testid="submit" type="submit">
+              Enviar
+            </Btn>
+          </BtnGroup>
+        </Form>
+      )}
+    </Modal>
+  );
+}
 
 VehicleForm.propTypes = {
   vehicle: PropTypes.shape({
