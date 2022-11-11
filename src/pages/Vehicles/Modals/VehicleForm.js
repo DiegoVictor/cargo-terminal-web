02d2a2d856a548f05bc54bb197cbcca80b2d9f7a
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-10 23:46:32 -0300

    Send vehicle form to API

--- src/pages/Vehicles/Modals/VehicleForm.js
+++ src/pages/Vehicles/Modals/VehicleForm.js
@@ -1,13 +1,39 @@
 import React, { useCallback } from 'react';
 import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 
+import api from '~/services/api';
 const schema = Yup.object().shape({
   model: Yup.string().required(),
   type: Yup.string().required(),
 });
 
 function VehicleForm({ vehicle, save, cancel }) {
+  const handleVehicleForm = useCallback(
+    (data) => {
+      (async () => {
+        try {
+          if (vehicle._id) {
+            await api.put(`/vehicles/${vehicle._id}`, data);
+            save({
+              _id: vehicle._id,
+              ...data,
+            });
+            toast.success('Veículo atualizado com sucesso!');
+          } else {
+            await api.post('vehicles', data);
+            save(data);
+            toast.success('Veículo criado com sucesso!');
+          }
+        } catch (err) {
+          toast.error('Não foi possivel criar o novo veículo');
+        }
+      })();
+    },
+    [vehicle, save]
+  );
+
 
 VehicleForm.propTypes = {
   vehicle: PropTypes.shape({
