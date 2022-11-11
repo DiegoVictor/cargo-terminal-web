7045ab4af6975b15b41203ce9029286e8eb77181
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-10 23:45:17 -0300

    Create validation

--- src/pages/Vehicles/Modals/VehicleForm.js
+++ src/pages/Vehicles/Modals/VehicleForm.js
@@ -1,4 +1,11 @@
 import React, { useCallback } from 'react';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  model: Yup.string().required(),
+  type: Yup.string().required(),
+});
+
 function VehicleForm({ vehicle, save, cancel }) {
 
 export default VehicleForm;
