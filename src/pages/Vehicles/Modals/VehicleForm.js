29cf4d32c751da75b3ba911d1456552ff4916931
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-10 23:45:43 -0300

    Configure component default props and types

--- src/pages/Vehicles/Modals/VehicleForm.js
+++ src/pages/Vehicles/Modals/VehicleForm.js
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import * as Yup from 'yup';
+import PropTypes from 'prop-types';
 
 const schema = Yup.object().shape({
   model: Yup.string().required(),
@@ -7,5 +8,17 @@ const schema = Yup.object().shape({
 });
 
 function VehicleForm({ vehicle, save, cancel }) {
+
+VehicleForm.propTypes = {
+  vehicle: PropTypes.shape({
+    _id: PropTypes.string,
+  }),
+  save: PropTypes.func.isRequired,
+  cancel: PropTypes.func.isRequired,
+};
+
+VehicleForm.defaultProps = {
+  vehicle: null,
+};
 
 export default VehicleForm;
