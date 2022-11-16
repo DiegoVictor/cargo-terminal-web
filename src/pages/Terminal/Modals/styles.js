bb02a710901434abca75584951d378e2de3dbfe4
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-16 11:04:13 -0300

    Create custom btn group and form with margin top

---
+++ src/pages/Terminal/Modals/styles.js
@@ -0,0 +1,10 @@
+import { ButtonGroup, Form } from 'react-bootstrap';
+import styled from 'styled-components';
+
+export const FormGroup = styled(Form.Group)`
+  margin-top: 20px;
+`;
+
+export const BtnGroup = styled(ButtonGroup)`
+  margin-top: 20px;
+`;
