5a9463c3f972806aa70689261eaebc5b819142d9
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-09-19 18:45:32 -0300

    refactor: remove Unform integration and simplify Input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,26 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReactInputMask from 'react-input-mask';
 
-import { useField } from '@unform/core';
-
-export default function Input({ name, ...rest }) {
-  const inputRef = useRef(null);
-  const { fieldName, defaultValue = '', registerField } = useField(name);
-
-  useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: inputRef.current,
-      path: 'value',
-    });
-  }, [fieldName, registerField]);
+export default function Input({ name, type = 'text', mask, error, ...rest }) {
+  if (mask) {
+    return (
+      <>
+        <ReactInputMask name={name} {...rest} />
+        {error && <span>{error}</span>}
+      </>
+    );
+  }
 
   return (
-    <ReactInputMask ref={inputRef} defaultValue={defaultValue} {...rest} />
+    <>
+      <input name={name} type={type} {...rest} />
+      {error && <span>{error}</span>}
+    </>
   );
 }
 
 Input.propTypes = {
   name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  error: PropTypes.string,
+};
+
+Input.defaultProps = {
+  error: null,
+  type: 'text',
 };
