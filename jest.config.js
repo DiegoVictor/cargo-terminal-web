772998fd7e5a38573d67d670e9eaf65d4491677e
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-09-23 20:24:55 -0300

    Update axios dependency to version 1.12.2 and adjust Jest module mapping

--- jest.config.js
+++ jest.config.js
@@ -81,6 +81,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^~/(.*)': '<rootDir>/src/$1',
+    '^axios$': 'axios/dist/node/axios.cjs',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
