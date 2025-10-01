edb55a2b12dfed55d648c1b91985189143657262
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-09-30 22:25:32 -0300

    Refactor Router component to use 'react-router' and replace Switch with Routes for improved routing structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import Drivers from '~/pages/Drivers';
 import Vehicles from '~/pages/Vehicles';
@@ -7,19 +7,17 @@ import Terminal from '~/pages/Terminal';
 import Travels from '~/pages/Travels';
 import Menu from '~/components/Menu';
 
-function Routes() {
+export default function Router() {
   return (
     <BrowserRouter>
       <Menu />
 
-      <Switch>
-        <Route path="/" exact component={Terminal} />
-        <Route path="/drivers" component={Drivers} />
-        <Route path="/travels" component={Travels} />
-        <Route path="/vehicles" component={Vehicles} />
-      </Switch>
+      <Routes>
+        <Route index element={<Terminal />} />
+        <Route path="/drivers" element={<Drivers />} />
+        <Route path="/travels" element={<Travels />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
-export default Routes;
