6b85e0804ded47b4c1dcb1c1399da751b7f6b008
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-10-02 23:00:08 -0300

    Add props spreading to Link component for enhanced flexibility

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link as A, useMatch } from 'react-router';
 import PropTypes from 'prop-types';
 
-export default function Link({ children, to }) {
+export default function Link({ children, to, ...props }) {
   const match = useMatch({
     path: to,
     exact: true,
   });
 
   return (
-    <div className={match ? 'active' : ''}>
+    <div {...props} className={match ? 'active' : ''}>
       <A to={to}>{children}</A>
     </div>
   );
