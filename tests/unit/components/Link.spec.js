703e4e5e31ed8d04c57029010fa74beb3ca4bd75
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-10-02 23:00:18 -0300

    Refactor Link component test to use MemoryRouter and Routes for improved routing structure

--- tests/unit/components/Link.spec.js
+++ tests/unit/components/Link.spec.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Router } from 'react-router';
+import { MemoryRouter, Route, Routes } from 'react-router';
 import { faker } from '@faker-js/faker';
 
-import history from '~/services/history';
 import Link from '~/components/Link';
 
 describe('Link component', () => {
   it('should be able to mark the link as active', () => {
     const linkLabel = faker.lorem.word();
-    const { container } = render(
-      <Router history={history}>
-        <Link to="/">{linkLabel}</Link>
-      </Router>
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Routes>
+          <Route
+            index
+            element={
+              <div>
+                <Link to="/" data-testid="link">
+                  {linkLabel}
+                </Link>
+              </div>
+            }
+          />
+        </Routes>
+      </MemoryRouter>
     );
 
-    expect(container.firstChild.classList.contains('active')).toBe(true);
+    expect(getByTestId('link')).toHaveClass('active');
   });
 });
